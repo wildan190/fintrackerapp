9924610eab9f1b1a028d444fb42b95b935b7bb57
Wildan Belfiore <mikewildan@outlook.com>
2025-04-04 01:25:50 +0700

    perubahan tema

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FinTracker App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        scaffoldBackgroundColor: Colors.teal.shade50,
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: Colors.teal.shade900,
+          ),
+          bodyLarge: TextStyle(fontSize: 16, color: Colors.teal.shade800),
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.teal,
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.teal,
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
       initialRoute: '/login', // Halaman awal aplikasi
       routes: {
         '/login': (context) => LoginPage(),
